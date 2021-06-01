a364028a9d79d03b87cd6f479f987c7be18df12f
Maharani Bening <79127156+Maharanibening@users.noreply.github.com>
2021-06-01 07:29:25 +0700

    Add files via upload

---
+++ Tugas-3/index.js
@@ -0,0 +1,125 @@
+console.log("Soal Nomor 1");
+let nilai = 30
+if(nilai >= 85){
+    console.log("nilainya diatas 85")
+}else if(nilai >= 75 && nilai < 85 ){
+    console.log("indeksnya B")
+}else if(nilai >= 65 && nilai < 75){
+    console.log("indeksnya C")
+}else if(nilai >= 55 && nilai < 65){
+    console.log("indeksnya D")
+}else if(nilai < 55){
+    console.log("indeksnya E")
+}
+
+
+console.log("Soal Nomor 2");
+
+let tanggal = 21;
+let bulan = 9;
+let tahun = 2001;
+switch(bulan) {
+  case 7:   { console.log('7 September 2004'); break; }
+  case 8:   { console.log('7 September 2003'); break; }
+  case 9:   { console.log('17 September 2001'); break; }
+  case 10:   { console.log('7 September 2000'); break; }
+  default:  { console.log('Belum lahir'); }}
+
+
+console.log("Soal Nomor 3”);
+n=3
+let jumlah = 0;
+for(let deret = 1; deret <= 3; deret++) {
+    let ht =''
+    for(let again = 1; again <= deret; again++){
+        ht += '#'
+    }
+  console.log(ht);
+}
+
+n=7
+let jumlahj = 0;
+for(let deret = 1; deret <= 7; deret++) {
+    let ht =''
+    for(let again = 1; again <= deret; again++){
+        ht += '#'
+    }
+  console.log(ht);
+}
+
+
+console.log("Soal Nomor 4”)
+for(var angka = 1; angka < 2; angka++) {
+    console.log('1 -' + ' I love programming');
+    console.log('2 -' + ' I love Javascript');
+    console.log('3 -' + ' I love VueJS');
+
+  } 
+
+  console.log('===');
+
+m=5
+for(var angka = 1; angka < 2; angka++) {
+    console.log('1 -' + ' I love programming');
+    console.log('2 -' + ' I love Javascript');
+    console.log('3 -' + ' I love VueJS');
+
+  } 
+
+  console.log('===');
+
+  for(var angka = 1; angka < 2; angka++) {
+  console.log('4 -' + ' I love programming');
+  console.log('5 -' + ' I love Javascript');}
+
+m=7
+for(var angka = 1; angka < 2; angka++) {
+    console.log('1 -' + ' I love programming');
+    console.log('2 -' + ' I love Javascript');
+    console.log('3 -' + ' I love VueJS');
+
+  } 
+
+  console.log('===');
+
+  for(var angka = 1; angka < 2; angka++) {
+  console.log('4 -' + ' I love programming');
+  console.log('5 -' + ' I love Javascript');
+  console.log('6 -' + ' I love VueJS');
+}
+
+  console.log('===');
+
+  for(var angka = 1; angka < 2; angka++) {
+  console.log('7 -' + ' I love programming');}
+
+m=10
+
+for(var angka = 1; angka < 2; angka++) {
+    console.log('1 -' + ' I love programming');
+    console.log('2 -' + ' I love Javascript');
+    console.log('3 -' + ' I love VueJS');
+
+  } 
+
+  console.log('===');
+
+  for(var angka = 1; angka < 2; angka++) {
+  console.log('4 -' + ' I love programming');
+  console.log('5 -' + ' I love Javascript');
+  console.log('6 -' + ' I love VueJS');
+}
+
+  console.log('===');
+
+  for(var angka = 1; angka < 2; angka++) {
+  console.log('7 -' + ' I love programming');
+  console.log('8 -' + ' I love Javascript');
+  console.log('9 -' + ' I love VueJS');}
+
+  console.log('===');
+
+  for(var angka = 1; angka < 2; angka++) {
+  console.log('10 -' + ' I love programming');}
+
+  
