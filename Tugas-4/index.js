8fbbc56e46f0053196c62f659c2317faa39577c6
Maharani Bening <79127156+Maharanibening@users.noreply.github.com>
2021-06-05 05:31:57 +0700

    Add files via upload

---
+++ Tugas-4/index.js
@@ -0,0 +1,47 @@
+//"Soal Nomor 1”
+
+function introduce() {
+return "Nama saya John, umur saya 30 tahun, alamat saya di Jalan Pelesiran, dan saya punya hobby yaitu Gaming!"
+}
+
+  var perkenalan = introduce();
+  console.log(perkenalan)
+  
+function hitung_huruf_vokal() {
+
+  }
+  var hitung_1 = 3
+
+var hitung_2 = 2
+
+console.log(hitung_1 , hitung_2) 
+
+// soal nomor 4
+function tampilkanAngka(angka = 1) {
+    return angka + -2
+  }
+  console.log(tampilkanAngka(0)) 
+
+  function tampilkanAngka2(angka = 1) {
+    return angka *0
+  }
+     console.log(tampilkanAngka2(1)) 
+
+function tampilkanAngka3(angka = 1) {
+        return angka *1
+      }
+         console.log(tampilkanAngka3(2)) 
+
+
+function tampilkanAngka4(angka = 1) {
+    return angka + 1
+  }
+     console.log(tampilkanAngka4(3)) 
+
+
+function tampilkanAngka5(angka = 1) {
+    return angka + 3
+  }
+     console.log(tampilkanAngka5(5)) 
+
+  
