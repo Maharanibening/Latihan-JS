c8c8e5435f32f8226da0f61b3be3924a8472b55c
Maharani Bening <79127156+Maharanibening@users.noreply.github.com>
2021-06-13 23:52:33 +0700

    Add files via upload

---
+++ Tugas-5/index.js
@@ -0,0 +1,26 @@
+console.log("Soal Nomor 3");
+
+var newObject = {
+    firstName: 'Muhammad',
+    lastName: "Iqbal Mubarok",
+    address: "Jalan Ranamanyar",
+    hobby: "playing football",
+};
+ 
+
+const {firstName, lastName, address, hobby} = newObject
+
+console.log(firstName, lastName, address, hobby)
+
+console.log("Soal Nomor 4");
+const west = ["Will", "Chris", "Sam", "Holly"]
+const east = ["Gill", "Brian", "Noel", "Maggie"]
+const combined = west.concat(east)
+
+console.log(combined) 
+
+console.log("Soal Nomor 5");
+const fullName = 'John Doe'
+ 
+const john = {fullName}
+
